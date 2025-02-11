a292817d94935416e25f9d41b2219cf549a36e0e
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-02-11 08:53:19 +0700

    fix: wrong config on gradle kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-val seleniumJavaVersion = "4.14.1"
-val seleniumJupiterVersion = "5.0.1"
-val webdrivermanagerVersion = "5.6.3"
-val junitJupiterVersion - "5.9.1"
-
 plugins {
     java
     id("org.springframework.boot") version "3.4.2"
@@ -27,6 +22,11 @@ configurations {
 repositories {
     mavenCentral()
 }
+
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
