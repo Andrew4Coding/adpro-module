9d341f5b30a0a5ded2492adf24f4c43fa0340747
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-03-07 21:11:36 +0700

    fix: wrong config in product and car controller test

--- src/test/java/id/ac/ui/cs/advprog/eshop/car/controller/CarControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/car/controller/CarControllerTest.java
@@ -14,14 +14,14 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import id.ac.ui.cs.advprog.eshop.car.model.Car;
 import id.ac.ui.cs.advprog.eshop.car.service.CarService;
 
-@SpringBootTest
+@WebMvcTest(CarController.class)
 @AutoConfigureMockMvc
 public class CarControllerTest {
     @Autowired
--- src/test/java/id/ac/ui/cs/advprog/eshop/product/controller/ProductControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/product/controller/ProductControllerTest.java
@@ -14,14 +14,14 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import id.ac.ui.cs.advprog.eshop.product.model.Product;
 import id.ac.ui.cs.advprog.eshop.product.service.ProductService;
 
-@SpringBootTest
+@WebMvcTest(ProductController.class)
 @AutoConfigureMockMvc
 public class ProductControllerTest {
     @Autowired
